ee5303a3ae788bf1f06b1f722bea28c8f1d737cf
sayeedajmal <sayeedajmala06@gmail.com>
2025-02-17 10:10:23 +0530

    inilitlize FamilyAuth

---
+++ familyauth/src/main/java/com/strong/familyauth/FamilyauthApplication.java
@@ -0,0 +1,13 @@
+package com.strong.familyauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyauthApplication.class, args);
+	}
+
+}
