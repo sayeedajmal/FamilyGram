c8d060dc678596defa784b4fed4c1efbf229f6fd
SAYEED AHMED <76242275+sayeedajmal@users.noreply.github.com>
2025-03-01 17:35:51 +0530

    Update SecurityConfig.java

--- familyauth/src/main/java/com/strong/familyauth/Security/SecurityConfig.java
+++ familyauth/src/main/java/com/strong/familyauth/Security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://familygram.web.app")); // Allow frontend
+        config.setAllowedOrigins(List.of("https://familygram.web.app","http://localhost:8081")); // Allow frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
